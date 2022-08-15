2c3e26cba91423ccccd65e9084aa0a6948994f0d
DrocheRrR <dmitriyspsh@gmail.com>
2022-08-15 14:16:08 +0300

    Create DZ9.py

---
+++ DZ9/DZ9.py
@@ -0,0 +1,22 @@
+"""""
+Дан словарь, создать новый словарь при помощи конструкции генератор словаря, поменяв местами ключ и значение.
+"""
+
+#def make_dict(length):
+    #dict_made = {}
+    #i = 1
+    #for i in range(length+1):
+        #key = input(f"Enter the {i} key: ")
+        #value =  input(f"Enter the {i} value: ")
+        #dict_made.items(key, value)
+        
+   #print (dict_made)
+    #return dict_made
+
+
+#len = int(input("Enter the dict length"))
+#make_dict(len)
+
+old_dict = {1: 'dr', '5':'34', '-65':"bc", None: 0.76, 102: "1323BB"}
+new_dict = {value: key for key, value in old_dict.items()}
+print(new_dict)
