64403f2e261b7777b740d184714856713c00f172
DrocheRrR <dmitriyspsh@gmail.com>
2022-09-05 10:48:48 +0300

    Update DZ17.py

--- DZ17/DZ17.py
+++ DZ17/DZ17.py
@@ -9,10 +9,15 @@ import json
 
 fieldname = ["Number","Name","Age"]
 with open ("data_name.json") as json_file:
-    csv_file = json.load(json_file)  
+    csv_file = json.load(json_file) 
+    print(csv_file) 
+    
 print(type(csv_file))
 with open("data_name_number.csv","w",encoding="utf-8",newline="") as file:
     file_writer = csv.DictWriter(file, fieldnames = fieldname)
     file_writer.writeheader()
     for i ,b in csv_file.items():
         file_writer.writerow({"Number": i, "Name": b[0], "Age":b[1]})
+
+    for i in file_writer:
+        print(i)
